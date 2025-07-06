1dbbb9a8034167ff9bf8e3364c8eeacb9ac5b886
Barun Parruck <barun.parruck@gmail.com>
2025-07-06 01:05:55 +0100

    Try not to break written code

--- clippy_lints/src/ifs_as_logical_ops.rs
+++ clippy_lints/src/ifs_as_logical_ops.rs
@@ -2,7 +2,7 @@ use clippy_utils::diagnostics::span_lint_and_sugg;
 use clippy_utils::source::{HasSession, SpanRangeExt, walk_span_to_context};
 use rustc_ast::LitKind;
 use rustc_errors::Applicability;
-use rustc_hir::*;
+use rustc_hir::{Expr, ExprKind};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::declare_lint_pass;
 
@@ -18,23 +18,25 @@ declare_clippy_lint! {
     ///
     /// ### Example
     /// ```no_run
-    /// # fn is_string_instrument(instrument: Instrument, is_in_orchestra: boolean) {
-    /// #   if is_in_orchestra { is_orchestral_string_instrument(instrument) } else { false }
-    /// # }
+    /// # fn a() -> bool { false }
+    /// fn b(b1: bool) -> bool {
+    ///  if b1 { a() } else { false}
+    /// }
     ///
     /// ```
     ///
     /// Could be written
     ///
     /// ```no_run
-    /// # fn is_string_instrument(instrument: Instrument, is_in_orchestra: boolean) {
-    /// #   is_in_orchestra && is_orchestral_string_instrument(instrument)
-    /// # }
+    /// # fn a() -> bool { false }
+    /// fn b(b1: bool) -> bool {
+    ///   b1 && a()
+    /// }
     /// ```
     #[clippy::version = "1.89.0"]
     pub IFS_AS_LOGICAL_OPS,
     nursery,
-    "default lint description"
+    "`if` conditions that can be rewritten as logical operators"
 }
 declare_lint_pass!(IfsAsLogicalOps => [IFS_AS_LOGICAL_OPS]);
 
@@ -43,11 +45,11 @@ impl<'tcx> LateLintPass<'tcx> for IfsAsLogicalOps {
         if let ExprKind::If(cond, cond_inner, Some(els)) = e.kind
             && let ExprKind::Block(if_block, _label) = cond_inner.kind
             // Check if the if-block has only a return statement
-            && if_block.stmts.len() == 0
+            && if_block.stmts.is_empty()
             && let Some(if_expr) = if_block.expr
             // And that the else block consists of only the boolean 'false'.
             && let ExprKind::Block(else_block, _label) = els.kind
-            && else_block.stmts.len() == 0
+            && else_block.stmts.is_empty()
             && let Some(else_expr) = else_block.expr
             && let ExprKind::Lit(lit) = else_expr.kind
             && matches!(lit.node, LitKind::Bool(false))
@@ -60,28 +62,24 @@ impl<'tcx> LateLintPass<'tcx> for IfsAsLogicalOps {
             && ctxt == else_expr.span.ctxt()
             && ctxt == lit.span.ctxt()
             && !ctxt.in_external_macro(cx.tcx.sess().source_map())
+            && let Some(lhs_snippet) = walk_span_to_context(cond.span, ctxt)
+                .and_then(|span| span.get_source_text(cx))
+            && let Some(rhs_snippet) = walk_span_to_context(if_expr.span, ctxt)
+                .and_then(|span| span.get_source_text(cx))
         {
-            if let Some(lhs_snippet) = walk_span_to_context(cond.span, ctxt)
-                .map(|span| span.get_source_text(cx))
-                .flatten()
-                && let Some(rhs_snippet) = walk_span_to_context(if_expr.span, ctxt)
-                    .map(|span| span.get_source_text(cx))
-                    .flatten()
-            {
-                span_lint_and_sugg(
-                    cx,
-                    IFS_AS_LOGICAL_OPS,
-                    e.span,
-                    "if expression that could be written as a logical and expression",
-                    "try",
-                    format!("{lhs_snippet} && {rhs_snippet}"),
-                    if ctxt.is_root() {
-                        Applicability::MachineApplicable
-                    } else {
-                        Applicability::MaybeIncorrect
-                    },
-                );
-            }
+            span_lint_and_sugg(
+                cx,
+                IFS_AS_LOGICAL_OPS,
+                e.span,
+                "if expression that could be written as a logical and expression",
+                "try",
+                format!("{lhs_snippet} && {rhs_snippet}"),
+                if ctxt.is_root() {
+                    Applicability::MachineApplicable
+                } else {
+                    Applicability::MaybeIncorrect
+                },
+            );
         }
     }
 }
